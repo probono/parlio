ff4739b67a2cba93abef1b8ff419b67dcb5890fd
Alberto Molpeceres <alberto.molpeceres@gmail.com>
2009-08-22 11:29:17 +0200

    get parlamentarian's interventions

--- lib/legebiltzarra/parliamentarian.rb
+++ lib/legebiltzarra/parliamentarian.rb
@@ -47,8 +47,7 @@ module Legebiltzarra
     def interventions
       int_url = "#{BASE_URL}/fichas/c_#{self.id}_int_SM.html"
       int_doc_list ||= Nokogiri::HTML(open(int_url).read)
-      #mostrar_sesion_comision('2007011950000001')
-      int_doc_list.search('div[@class="indentar2"]').map{ |p| Intervention.new(p.content.match(/mostrar_sesion_comision\('(.*)'\)/)[0]) }
+      int_doc_list.search('div[@class="indentar2"]/ul/li/a/@onclick').map{ |p| Intervention.new(p.content.match(/\d+/)[0]) }
     end
   end
 end
