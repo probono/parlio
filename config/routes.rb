30b888a42a45889c39810df6256d42558e5d03ce
Aitor García <aitor.garcia@gmail.com>
2009-08-22 14:08:25 +0200

    page controller for legal, contact and about page

--- config/routes.rb
+++ config/routes.rb
@@ -31,13 +31,13 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
+  map.with_options(:controller => 'page') do |page|
+    ["contact", "about", "legal"].each do |name|                    
+      page.send name,  name, :action => name
+    end
+  end 
 
-  # See how all your routes lay out with "rake routes"
 
-  # Install the default routes as the lowest priority.
-  # Note: These default routes make all actions in every controller accessible via GET requests. You should
-  # consider removing the them or commenting them out if you're using named routes and resources.
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
+  map.root :controller => "home"
+
 end
