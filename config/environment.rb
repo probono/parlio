ca448a36482e5def109ea2ca87b1ccdb2258a7b8
Alberto Molpeceres <alberto.molpeceres@gmail.com>
2009-08-22 09:16:41 +0200

    fixed typo in legebiltzarra declaration

--- config/environment.rb
+++ config/environment.rb
@@ -14,7 +14,7 @@ Rails::Initializer.run do |config|
   # Add additional load paths for your own custom dirs
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
 
-  config.gem "probono-legebiltzarra", :lig => 'legebiltzarra', :version => '0.1', :source => "http://gems.github.com"
+  config.gem "probono-legebiltzarra", :lib => 'legebiltzarra', :version => '0.1.0', :source => "http://gems.github.com"
   # Specify gems that this application depends on and have them installed with rake gems:install
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
