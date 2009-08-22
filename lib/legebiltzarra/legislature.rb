efa88a2cc3d2b2bb1b9ac15e43b4fb386d10b1f9
Alberto Molpeceres <alberto.molpeceres@gmail.com>
2009-08-22 13:11:47 +0200

    get parties information

--- lib/legebiltzarra/legislature.rb
+++ lib/legebiltzarra/legislature.rb
@@ -1,18 +1,24 @@
 module Legebiltzarra
   class Legislature                        
     CURRENT_LEGISLATURE="LGA"
-    CURRENT_PARLIAMENTARIANS="ACT"
+    ACTIVE="ACT"
     BASE_URL = "http://www.parlamento.euskadi.net"
     
     attr_accessor :id, :url
   
-    def initialize(id=CURRENT_PARLIAMENTARIANS)
+    def initialize(id=ACTIVE)
       @id = id                   
       @url = "#{BASE_URL}/comparla/c_comparla_alf_#{self.id}.html"
     end
   
     def parliamentarians
       document.search('td[@class="miembro_persona"]/a/@href').map { |p| Parliamentarian.new(p.content.match(/\/fichas\/c_(.*).html/)[1]) } rescue []
+    end
+    
+    def parties(id=ACTIVE)
+      parties_url = "#{BASE_URL}/c_comorga_gru_#{self.id}_SM.html"
+      parties_document ||= Nokogiri::HTML(open(parties_url).read)
+      parties_document.search('div[@id="grupos"]/table/tr').map { |p| Party.new(p) } rescue []      
     end
   
   
---
+++ lib/legebiltzarra/party.rb
@@ -0,0 +1,35 @@
+module Legebiltzarra
+  class Party
+    BASE_URL = "http://www.parlamento.euskadi.net"
+    attr_accessor :content
+
+    def initialize(content)
+      @content = content
+    end
+
+    def group_name
+      @content.search('td')[0].content
+    end
+        
+    def acronym
+      @content.search('td')[1].content
+    end
+    
+    def sites
+      @content.search('td')[2].content
+    end
+    
+    def name
+      @content.search('td')[3].content
+    end
+
+    def url
+      @content.search('td')[4].search('a/@href')[0]
+    end
+                
+    def logo
+      BASE_URL + @content.search('td')[4].search('img/@src')[0]
+    end
+    
+  end
+end
