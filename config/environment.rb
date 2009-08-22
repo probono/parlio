6950006369c2fcf4269645b6dc376b2b55cae846
Roberto Salicio <roberto.salicio@gmail.com>
2009-08-22 20:41:25 +0200

    remove warning

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,5 @@
 # Be sure to restart your server when you modify this file
+I_KNOW_I_AM_USING_AN_OLD_AND_BUGGY_VERSION_OF_LIBXML2=true
 
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
