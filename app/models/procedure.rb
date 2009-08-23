b9d6f0d0f5b870399cd0c80ce0774fe3c2406e6f
Alberto Molpeceres <alberto.molpeceres@gmail.com>
2009-08-23 17:31:11 +0200

    showing procedures as they should be shown

--- app/models/procedure.rb
+++ app/models/procedure.rb
@@ -2,4 +2,13 @@ class Procedure < ActiveRecord::Base
   
   belongs_to :initiative
   
+  def procedure_date
+    value = self.title[0, self.title.index('/')]
+    Date.strptime(value, "%d.%m.%Y")
+  end
+
+  def name
+    self.title[self.title.index('/') + 1, self.title.size - 1]
+  end
+    
 end
