a173834477c1695d0d8c9ccb97f582ec883ad6b9
Roberto Salicio <roberto.salicio@gmail.com>
2009-08-23 23:56:31 +0200

    most_active_parliamentarians tag

--- app/controllers/taggings_controller.rb
+++ app/controllers/taggings_controller.rb
@@ -1,8 +1,10 @@
 class TaggingsController < ApplicationController
   before_filter :find_taggings, :only => [:show]
 
-  def show      
+  def show
     @tag = Tag.find_by_name(params[:tag])
+    @most_active_parliamentarians = most_active_parliamentarians(params[:tag])
+
     respond_to do |wants|
       wants.html
       wants.xml  { render :xml => @tagging }
@@ -10,6 +12,10 @@ class TaggingsController < ApplicationController
   end
 
   private
+    def most_active_parliamentarians(tag)
+      Initiative.find_tagged_with(tag).group_by(&:parliamentarian).sort_by{|x| x[1].size}.reverse.first(3)
+    end
+
     def find_taggings
       @taggings = Initiative.find_tagged_with(params[:tag]).paginate :per_page => 5,  :page => params[:page]
     end
