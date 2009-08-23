6d9d91bf1cd15611c0b498487a840ba31fe5ccd3
Alberto Molpeceres <alberto.molpeceres@gmail.com>
2009-08-23 17:21:19 +0200

    selecting current tab in main menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,19 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  
+  def main_tab_class(tab)
+    c = controller.controller_name
+    puts "controller #{c}"
+
+    if tab == 'parliament'
+      'class="active"' if ['parliamentarians', 'parties'].include?(c)
+    elsif tab == 'activity'
+        'class="active"' if ['activity', 'initiatives', 'taggings', 'interventions', 'commisions', 'topics'].include?(c)
+    elsif tab == 'about'
+      'class="active"' if c == 'page'
+    else
+      ''
+    end
+  end
+  
 end
