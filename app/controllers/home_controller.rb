bd6be733b1bd7dafb84df6ce31240a5458096587
Aitor García <aitor.garcia@gmail.com>
2009-08-23 21:32:07 +0200

    Home and taggings addiction

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
-  def index
+  def index      
+    @most_active = Topic.most_active
+    tuples = Tagging.count(:all, :group => "tag_id", :order => "count(*) DESC", :limit=> 10)
+    returning @most_active_tags = [] do
+      tuples.each{|tuple| @most_active_tags << Tag.find(tuple[0])}
+    end
+    @most_active_parliamentarians= Parliamentarian.most_active
   end
 
 end
