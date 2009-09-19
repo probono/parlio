789f945fd03e358ad1955b18c78ed03630f5a2bc
Alberto Molpeceres <alberto.molpeceres@gmail.com>
2009-09-19 11:37:58 +0200

    initiatives could have nil parliamentarian

--- app/helpers/initiative_helper.rb
+++ app/helpers/initiative_helper.rb
@@ -1,2 +1,7 @@
 module InitiativeHelper
+  
+  def initiative_author(i)
+    "#{link_to i.parliamentarian.full_name, parliamentarian_path(i.parliamentarian), :class=>"author"}" +
+       "  #{I18n.t("info.de")} #{link_to i.parliamentarian.party.party_acronym, party_path(i.parliamentarian.party), :class=>"party_name"}" if i.parliamentarian
+  end
 end
