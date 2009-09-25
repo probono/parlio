9e3693299a355b94ee8c99417a632d2bafa1b43f
Alberto Molpeceres <alberto.molpeceres@gmail.com>
2009-09-25 22:17:18 +0200

    showing initiatives in comissions' detail page

--- app/controllers/commisions_controller.rb
+++ app/controllers/commisions_controller.rb
@@ -1,18 +1,23 @@
 class CommisionsController < ApplicationController
   meta :title => "Parlio - Comisiones Actuales en el Parlamento Vasco"
 
+  before_filter :all_commissions
   before_filter :find_commision, :only => [:show]
 
   def index
-    @commisions = Commision.all
     @commision  = Commision.first
 
     render :action => :show
   end
 
   def show
-    @commisions = Commision.all
-    meta :title => "Parlio - Debates realizados en la comisión de #{@commision.name}"
+    meta :title => "Parlio - Debates e iniciativas de la comisión de #{@commision.name}"
+
+    @page = params[:page] || 1
+
+    @activity = @commision.interventions + @commision.initiatives
+    @activity = @activity.sort_by { |e| if e.instance_of? Initiative; e.initiative_date; else e.session_date; end }.reverse
+    @activity = @activity.paginate :page => @page, :per_page => 8
 
     respond_to do |wants|
       wants.html # show.html.erb
@@ -21,6 +26,11 @@ class CommisionsController < ApplicationController
   end
 
   private
+  
+    def all_commissions
+      @commisions = Commision.all
+    end
+    
     def find_commision
       @commision = Commision.find(params[:id])
     end
