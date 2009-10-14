953ec06b48147e7f8d28ba7560e3c2b8deff2e8d
molpe <alberto.molpeceres@gmail.com>
2009-10-14 17:57:34 +0200

    temp html for search results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
 
-  SOURCES = ['Parliamentarian', 'Initiative', 'Intervention']
+  SOURCES = ['Initiative', 'Intervention']
 
   def search
     @page = params[:page] || '1'
@@ -11,7 +11,7 @@ class SearchController < ApplicationController
       @matches << source.site_search(@term)
     end
     @matches = @matches.flatten.sort {|x,y| y.created_at <=> x.created_at }
-
+    @parliamentarians = Parliamentarian.site_search(@term)
     @matches = @matches.paginate :page => @page, :per_page => 25
     respond_to do |format|
       format.html 
